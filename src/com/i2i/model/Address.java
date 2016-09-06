e472bb2390d7254e096f15805611ae32ea05e3c5
praveen-ece <mrpravin94@gmail.com>
2016-09-06 19:37:53 +0530

    Address Model, DAO, service added

---
+++ src/com/i2i/model/Address.java
@@ -0,0 +1,147 @@
+package com.i2i.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.i2i.model.Employee;
+
+
+/**
+ * Model class for Address
+ * Setter and Getter methods for the class variables
+ * Many to One mapping is established for Employee model class
+ * 
+ * @author Praveenkumar
+ * 
+ * @created 2016-09-06
+ */
+
+
+@Entity
+@Table(name="address")
+public class Address {
+    
+	@Id
+	@Column(name="id")
+	private int addressId;
+	
+	@Column(name="street")
+	private String street;
+	
+	@Column(name="city")
+	private String city;
+
+	@Column(name="state")
+	private String state;
+
+	@Column(name="country")
+	private String country;
+
+	@Column(name="pincode")
+	private int pincode;
+
+	@Column(name="phonenumber")
+	private int phoneNumber;
+
+	@Column(name="email")
+	private String eMail;
+
+	@Column(name="type")
+	private String addressType;
+	
+	@ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "employee_id")
+	private Employee employee;
+	
+	public Address() {
+	}
+
+	public int getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public String getAddressType() {
+		return addressType;
+	}
+
+	public void setAddressType(String addressType) {
+		this.addressType = addressType;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+	public String toString() {
+		return (street + city + state);
+	}
+}
---
+++ src/com/i2i/service/AddressService.java
@@ -0,0 +1,112 @@
+package com.i2i.service;
+
+import java.util.List;
+
+
+import com.i2i.dao.AddressDao;
+import com.i2i.exception.DataException;
+import com.i2i.model.Address;
+
+/**
+ * <p>
+ * Service class which does validations with the user input of address details.
+ * Passes values to the Dao class to carry out manipulations.
+ * Throws error messages in case of occurrence of any exceptions.
+ * </p>
+ *
+ * @author Praveenkumar
+ *
+ * @created 2016-08-15
+ */
+public class AddressService {
+    AddressDao addressDao = new AddressDao();
+    
+    /**
+     * <p>
+     * This method checks the presence of address ID in the database.
+     * Passes the value to its dao class to insert if not present. 
+     * </p>
+     * 
+     * @param address
+     *       model object that stores the address data associated with model.
+     * @return boolean
+     *       gives the status of the insertion into the database.
+     * @throws DataException
+     *       throws error message if problem arises with inserting the data in the database.
+     */
+    public boolean addAddress(Address address) throws DataException {    	
+    	return addressDao.insertAddress(address);
+    	
+    }
+    
+    
+    /**
+     * <p>
+     * This method checks the presence of address ID in the database.
+     * Passes the value to its dao class to insert if not present. 
+     * </p>
+     * 
+     * @param address
+     *       model object that stores the address data associated with model.
+     * @return boolean
+     *       gives the status of the insertion into the database.
+     * @throws DataException
+     *       throws error message if problem arises with inserting the data in the database.
+     */
+    public boolean updateAddress(Address address) throws DataException {    	
+    	return addressDao.modifyAddress(address);
+    	
+    }
+    
+    /**
+     * <p>
+     * This method checks the presence of address ID in the database.
+     * Passes the value to its dao class to delete if present.
+     * </p>
+     * 
+     * @param addressId
+     *       contains the ID of the address.
+     * @return boolean
+     *       gives the status of the deletion from the database.
+     * @throws DataException
+     *       throws error message if problem arises with deleting the data in the database.
+     */
+    public boolean deleteAddress(int addressId) throws DataException {
+        if (addressDao.findAddress(addressId) != null) {
+            return addressDao.removeAddress(searchAddress(addressId));
+        }
+        return false;
+    }
+    
+    /**
+     * <p>
+     * This method passes the address ID to its dao class to search in the database.
+     * Returns the model object of the address to its controller to display.
+     * </p>
+     *
+     * @param addressId
+     *       contains the ID of the address.
+     *       
+     * @return object
+     *       gives the appropriate address object for the corresponding address ID.
+     * @throws DataException
+     *       throws error message if problem arises with searching the data in the database.
+     */
+    public Address searchAddress(int addressId) throws DataException {
+        return addressDao.findAddress(addressId);
+    }
+    
+    /**
+     * <p>
+     * This method retrieves the Address data from the records and returns the list of data to display.
+     * </p>
+     * 
+     * @return list
+     *       Gives the list of address details retrieved from the database.
+     * @throws DataException
+     *       throws error message if problem arises with retrieving list of data from the database.
+     */
+    public List<Address> displayAddresss() throws DataException {
+        return addressDao.retrieveAddresss();
+    }
+}
